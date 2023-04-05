90e499a2e41a8d307f0a17af92b4cf4ca982b40b
Lana <tiushkievich00@mail.ru>
2023-04-05 18:37:59 +0200

    init: start project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Users from "./components/users";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
